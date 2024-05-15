3eba838de34e1f5690759a7e114d07a2dd7f23b3
ilkinsufi <ilkinsufi@gmail.com>
2024-05-15 22:04:53 +0400

    Focus button add

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -2,17 +2,32 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { TextInput } from "react-native-paper";
 import { colors } from "../utils/colors";
+import { RoundedButton } from "../components/RoundedButton";
 
 export const Focus = () => {
   const [subject, setSubject] = useState(null);
-  console.log(subject);
+
   return (
     <View style={styles.container}>
       <View style={styles.input}>
         <TextInput
+          style={{
+            backgroundColor: "white",
+            borderRadius: 2,
+            padding: "offset",
+            marginBottom: "offset",
+            fontSize: 18,
+            fontWeight: "bold",
+            color: colors.black,
+            flex: 1,
+            marginRight: 10,
+          }}
           onChangeText={setSubject}
           label="Nəyə fokuslanmaq istəyirsən?"
         />
+        <View style={styles.button}>
+          <RoundedButton title="+" size={50} />
+        </View>
       </View>
     </View>
   );
@@ -22,12 +37,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  input: {
-    flex: 0.5,
-    padding: 25,
-    justifyContent: "top",
+  button: {
+    justifyContent: "center",
   },
-  text: {
-    color: colors.white,
+  input: {
+    padding: 20,
+    justifyContent: "top",
+    flexDirection: "row",
   },
 });
